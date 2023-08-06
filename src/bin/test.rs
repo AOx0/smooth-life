d1da9df08df68e3c397e2302c9266585fd96a066
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-08-06 17:51:47 -0600

    Progress

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -4,7 +4,7 @@ fn main() {
     #[cfg(feature = "dhat")]
     let _profiler = dhat::Profiler::new_heap();
 
-    let grid: Grid<10, 10> = Grid::new().into_rand_state();
+    let grid: Grid<2500, 2500> = Grid::new().into_rand_state();
 
     println!("{:}", grid);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,37 @@
 #![warn(clippy::pedantic)]
 #![deny(rust_2018_idioms, unsafe_code)]
 
+use std::borrow::Cow;
+
 #[cfg(feature = "dhat")]
 #[global_allocator]
 pub static ALLOC: dhat::Alloc = dhat::Alloc;
 
 #[derive(Debug)]
-pub struct Grid<const W: usize, const H: usize>([[f32; W]; H]);
+pub struct Grid<const W: usize, const H: usize> {
+    grid: Vec<Vec<f32>>,
+    alpha: f32,
+    birdth_rate: (f32, f32),
+    death_rate: (f32, f32),
+}
 
 const LEVELS: &[char] = &[' ', '-', '=', '*', 'c', 'o', 'a', 'A', '@', '#'];
 const LEVELS_LEN: usize = LEVELS.len();
 
 impl<const W: usize, const H: usize> Default for Grid<W, H> {
     fn default() -> Self {
-        Grid([[0.; W]; H])
+        Grid {
+            grid: vec![vec![0.; W]; H],
+            alpha: 5.,
+            birdth_rate: (0.5, 0.5),
+            death_rate: (0.5, 0.5),
+        }
     }
 }
 
 impl<const W: usize, const H: usize> std::fmt::Display for Grid<W, H> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.iter().try_for_each(|row| {
+        self.grid.iter().try_for_each(|row| {
             row.iter().try_for_each(|col| {
                 let i = (col * (LEVELS_LEN as f32 - 1.)).round() as usize % LEVELS_LEN;
                 write!(f, "{:} ", LEVELS[i])
@@ -39,11 +51,26 @@ impl<const W: usize, const H: usize> Grid<W, H> {
         Grid::default()
     }
 
+    #[inline(always)]
+    fn sigma1(x: f32, a: f32, alpha: f32) -> f32 {
+        1. / 1. + (-(x - a) * 4. / alpha).exp()
+    }
+
+    #[inline(always)]
+    fn sigma2(x: f32, a: f32, b: f32, alpha: f32) -> f32 {
+        Self::sigma1(x, a, alpha) * (1. - Self::sigma1(x, b, alpha))
+    }
+
+    #[inline(always)]
+    fn sigmaM(x: f32, y: f32, m: f32, alpha: f32) -> f32 {
+        x * (1. - Self::sigma1(m, 0.5, alpha)) + y * Self::sigma1(m, 0.5, alpha)
+    }
+
     #[must_use]
     pub fn into_rand_state(mut self) -> Self {
         use rand::prelude::*;
         let mut rng = rand::thread_rng();
-        self.0.iter_mut().for_each(|row| {
+        self.grid.iter_mut().for_each(|row| {
             row.iter_mut()
                 .for_each(|col| *col = rng.gen_range((0.)..=(1.)))
         });
